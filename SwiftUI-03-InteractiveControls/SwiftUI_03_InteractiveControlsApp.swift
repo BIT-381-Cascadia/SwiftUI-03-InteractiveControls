f2bb8d3e14c4712d4f6693cdeeea6736a53f96cb
Mike Panitz <michpa@hotmail.com>
2021-04-11 18:10:39 -0700

    Initial Commit

---
+++ SwiftUI-03-InteractiveControls/SwiftUI_03_InteractiveControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_03_InteractiveControlsApp.swift
+//  SwiftUI-03-InteractiveControls
+//
+//  Created by Mike Panitz on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_03_InteractiveControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
